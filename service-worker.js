d3a364c814fe7e8353f514c697c900b00380bdeb
Jacob Wilson <jacob@jacobwilson.net>
2022-11-01 22:36:46 -0400

    dd update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34aae093b05e56b19b72f89b5a104727.js"
+  "/precache-manifest.665c3fb9d149c9f2b4887b7e783712a8.js"
 );
 
 self.addEventListener('message', (event) => {
